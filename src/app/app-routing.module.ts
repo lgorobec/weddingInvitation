cbaa00915ba567ed9c23c7276365e22d89f73943
Larisa Gorobets <larisa.gorobets.1@gmail.com>
2020-02-19 20:53:50 +0300

    Edit travis

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LetterComponent} from './components/letter/letter.component';
+import {InvitationComponent} from './components/invitation/invitation.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'letter/:names', component: LetterComponent },
+  { path: 'invitation', component: InvitationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LetterComponent } from './components/letter/letter.component';
+import { InvitationComponent } from './components/invitation/invitation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LetterComponent,
+    InvitationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
